9116980a4bf598a2b6eaf3777b5fb3b2c06f89fc
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-10 18:51:32 +0100

    Beginning on ContentDelivery

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            backgroundImage: {
+                'logo_zwart': "url('/img/logo_jb_zwart.png')",
+                'logo_wit': "url('/img/logo_jb_wit.png')",
+            },
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
