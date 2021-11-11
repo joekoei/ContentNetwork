59b718a87bf2d88378a22606907048fc1d4f8fb8
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-11 18:48:27 +0100

    Fixed admin panel navbar. letsgo

---
+++ resources/views/layouts/admin-nav.blade.php
@@ -0,0 +1,23 @@
+<div class="flex justify-between h-5">
+    <div class="flex">
+        <!-- Logo -->
+        <div class="flex-shrink-0 flex items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Administration Panel
+            </h2>
+        </div>
+
+        <!-- Navigation Links -->
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <x-nav-link>
+                Landingspage
+            </x-nav-link>
+            <x-nav-link>
+                Users
+            </x-nav-link>
+            <x-nav-link>
+                Stats about content network
+            </x-nav-link>
+        </div>
+    </div>
+</div>
