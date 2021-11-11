c485a0664ffe92bb7bf9d87db3fa0ef146b066bc
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-11 11:11:45 +0100

    Just 'fase 1' finishing

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -26,6 +26,26 @@
                         </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            1
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">test.png</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{\Illuminate\Support\Facades\Date::now()}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900"><a href="">Copy</a></div>
+                            </td>
+                        </tr>
                         </tbody>
                     </table>
 
