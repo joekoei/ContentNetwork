c141faa808b18921347aef69d3ef19b682dca2af
Joey Blommers <joeyblommers2004@gmail.com>
2021-12-13 00:48:04 +0100

    Fixed some minor things, Maybe going live tomorrow

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Statistieken user. (Fase 2)
+                    <h1>Jij hebt <span class="text-gray-500">{{\App\Models\Media::all()->where('user_id', '=', \Illuminate\Support\Facades\Auth::user()->id)->count()}}</span> bestanden geupload!</h1>
                 </div>
             </div>
         </div>
